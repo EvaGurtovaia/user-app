e42dc74e955854f32445c3ba2d075cd573a6aa40
EvaGurtovaia <eva.gurtovaya@gmail.com>
2022-01-08 14:55:18 -0600

    Added some styling

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,10 +1,12 @@
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
+
 const AddUser = (props) => {
     const addUserHandler = (event) => {
         event.preventDefailt();
-
     };
     return (
-        <div>
+        <Card className={classes.input}>
             <form onSubmit={addUserHandler}>
                 <label htmlFor="username">Username</label>
                 <input id="username" type="text"></input>
@@ -12,7 +14,7 @@ const AddUser = (props) => {
                 <input id="age" type="number"></input>
                 <button>Add user</button>
             </form>
-        </div>
+        </Card>
     );
 };
 
