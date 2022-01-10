ddc5dc783c60821eb392bbd919a1740bb73014d0
EvaGurtovaia <eva.gurtovaya@gmail.com>
2022-01-10 14:25:05 -0600

    Adding some validation logic

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -5,16 +5,25 @@ import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
     const [enteredUsername, setEnteredUsername] = useState("");
-    const [enteredAge, setEntereAge] = useState("");
+    const [enteredAge, setEnteredAge] = useState("");
 
     const usernameChangeHandler = (event) => {
         setEnteredUsername(event.target.value);
     };
     const ageChangeHandler = (event) => {
-        setEntereAge(event.target.value);
+        setEnteredAge(event.target.value);
     };
     const addUserHandler = (event) => {
         event.preventDefailt();
+        if (enteredUsername.trim().length === 0 || enteredAge) {
+            return;
+        }
+        if (+enteredAge < 1) {
+            return;
+        }
+
+        setEnteredUsername("");
+        setEnteredAge("");
     };
     return (
         <Card className={classes.input}>
@@ -23,12 +32,14 @@ const AddUser = (props) => {
                 <input
                     id="username"
                     type="text"
+                    value={enteredUsername}
                     onChane={usernameChangeHandler}
                 ></input>
                 <label htmlFor="age">Age</label>
                 <input
                     id="age"
                     type="number"
+                    value={enteredAge}
                     onChange={ageChangeHandler}
                 ></input>
                 <Button>Add user</Button>
